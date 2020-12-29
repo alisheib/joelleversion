4a621ecd0278f46a2d43f2327a02909ff8e6cf43
JoelleMahfoud <58190001+JoelleMahfoud@users.noreply.github.com>
2020-12-30 00:17:12 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,32 +3,41 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
 	"User",
 
-	new mongoose.Schema({
-		username: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		dateOfBirth: {
-			type: String,
-			required: true,
-		},
-		enrolledCourses: {
-			type: Array,
-			required: false,
-		},
-		createdCourses: {
-			type: Array,
-			required: false,
-		},
-	})
+new mongoose.Schema({
+	name:{
+      type: String,
+      required: true,
+    },  
+	username: {
+      type: String,
+      required: true,
+    },
+	email:{ 
+      type: String,
+      required: true,
+    },
+	country:{
+      type: String,
+      required: true,
+    },
+	password: { 
+      type: String,
+      required: true,
+    },
+	dateOfBirth:{ 
+      type: Date,
+      required: true,
+    },
+ 	enrolledCourses: { 
+      type: Array,
+      required: false,
+    },
+	createdCourses: { 
+      type: Array,
+      required: false,
+    },
+  })
 );
+
 
 module.exports = User;
