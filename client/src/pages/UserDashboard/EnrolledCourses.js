27e51262b1fc5bfed14f600c33d498c4fe5d3c07
Ali Sheib <alisheib07@gmail.com>
2021-01-10 14:55:20 +0200

    Working enrolled courses

--- client/src/pages/UserDashboard/EnrolledCourses.js
+++ client/src/pages/UserDashboard/EnrolledCourses.js
@@ -1,13 +1,91 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Card from "../../components/Course/Card";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import img1 from "../../assets/img/dummyimg.png";
 import SideBar from '../../components/General/Sidebar'
+class EnrolledCourses extends Component {
+	initialState = {
+		courses: "",
+	};
 
-// TODO implement similar to allCourses
-export default function EnrolledCourses() {
-	return (
-		<div>
+
+	constructor(props) {
+		super(props);
+		this.state = this.initialState;
+		this.fetchUserInfo = this.fetchUserInfo.bind(this);
+		this.displayCourse = this.displayCourse.bind(this);
+	}
+
+
+	componentDidMount = () => {
+		this.fetchUserInfo();
+	};
+
+	displayCourse = (courses) => {
+		if(courses.length === 0){
+			return (
+				<h1>No EnrolledCourses</h1>
+			)
+		}
+		return courses?.length > 0 && courses.map((course, index) => (
+			<div key={index} className="col-md-4">
+				<Card courseID={course._id} imgsrc={img1} title={course.name} par={course.type} />
+			</div>
+		));
+	};
+
+	fetchUserInfo = () => {
+  
+		//let courseId =this.state.courseID;
+		if (localStorage.getItem("ijam-user-token")) {
+		  let token = localStorage.getItem("ijam-user-token");
+		  console.log(token);
+		  let config = {
+			headers: {
+			  "x-access-token": token,
+			},
+		  };
+		  axios
+			.get("/api/users/token", config) // the proxy i created is set to our local host
+			.then((response) => {
+			  let myCourses = response.data.enrolledCourses;
+			  this.setState({courses: myCourses})
+			})
+			.catch(() => {
+			  alert("Error retreiveing Data");
+			});
+		
+	  };
+	};
+
+	
+
+
+
+
+	render() {
+
+
+		return (
+			
+			
+				
+			<div>
 			<SideBar />
-			<h3 className="h22">Courses Enrolled</h3>
-		</div>
-
-	)
+			<h3 className="h22">Enrolled Courses</h3>
+			<div className="content">
+				<div className="container-fluid d-flex justify-content-center">
+					<div className="row">
+						<div className="courses">
+							{this.displayCourse(this.state.courses)}
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		);
+	}
 }
+
+export default EnrolledCourses
